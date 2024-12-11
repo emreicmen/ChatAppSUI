5283e52266bb87c1f0d5a42c228c7edba586f186
Emre İçmen <icmenemre@gmail.com>
2024-12-11 14:44:50 +0300

    Initial Commit

---
+++ ChatAppSUI/ChatAppSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatAppSUIApp.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 11.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatAppSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

bc5aa5fae6b481f63dc49075a5103ea761830864
Emre İçmen <icmenemre@gmail.com>
2024-12-11 19:04:36 +0300

    Settings View designed

---
+++ ChatAppSUI/View/Settings/SettingCell.swift
@@ -0,0 +1,18 @@
+//
+//  SettingCell.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 11.12.2024.
+//
+
+import SwiftUI
+
+struct SettingCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingCell()
+}
---
+++ ChatAppSUI/View/Settings/SettingsHeaderView.swift
@@ -0,0 +1,8 @@
+//
+//  SettingsHeaderView.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 11.12.2024.
+//
+
+import Foundation

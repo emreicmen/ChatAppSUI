2469cfb9fa2896f5230e2e2402a63ea694e18bc9
Emre İçmen <icmenemre@gmail.com>
2024-12-11 16:25:08 +0300

    Authentication screens (Login and registration screens) designed

---
+++ ChatAppSUI/Utils/CustomTextField.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTextField.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 11.12.2024.
+//
+
+import SwiftUI
+
+struct CustomTextField: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTextField()
+}

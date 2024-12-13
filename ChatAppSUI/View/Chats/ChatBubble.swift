322281daf326e2f9f11b7913a91f13b5b467f097
Emre İçmen <icmenemre@gmail.com>
2024-12-13 17:15:14 +0300

    Chat bubble created

---
+++ ChatAppSUI/View/Chats/ChatBubble.swift
@@ -0,0 +1,18 @@
+//
+//  ChatBubble.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 13.12.2024.
+//
+
+import SwiftUI
+
+struct ChatBubble: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatBubble()
+}
---
+++ ChatAppSUI/View/Chats/MessageView.swift
@@ -0,0 +1,18 @@
+//
+//  MessageView.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 13.12.2024.
+//
+
+import SwiftUI
+
+struct MessageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MessageView()
+}

69483abad16c8607cac9a76c2ebcabbe73b4696e
Emre İçmen <icmenemre@gmail.com>
2024-12-13 16:05:15 +0300

    Custon Search Bar designed

---
+++ ChatAppSUI/Utils/Extensions.swift
@@ -0,0 +1,8 @@
+//
+//  Extensions.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 13.12.2024.
+//
+
+import Foundation
---
+++ ChatAppSUI/Utils/SearchBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchBar.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 13.12.2024.
+//
+
+import SwiftUI
+
+struct SearchBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchBar()
+}

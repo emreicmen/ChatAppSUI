91efa855ac73f0ba60f50cb257a8a12aa0089fde
Emre İçmen <icmenemre@gmail.com>
2024-12-16 12:27:45 +0300

    Custom input View designed

---
+++ ChatAppSUI/View/Chats/CustomInputView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomInputView.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 16.12.2024.
+//
+
+import SwiftUI
+
+struct CustomInputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomInputView()
+}

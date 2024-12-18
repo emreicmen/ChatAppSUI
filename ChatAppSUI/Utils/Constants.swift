daeb44f9b4967c6eae9eb7bb7d424ff164b0bdbf
Emre İçmen <icmenemre@gmail.com>
2024-12-18 12:16:16 +0300

    Display users in NewMessageView

---
+++ ChatAppSUI/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 18.12.2024.
+//
+
+import Foundation
+import Firebase
+
+let COLLECTION_USERS = Firestore.firestore().collection("Users")
+
---
+++ ChatAppSUI/ViewModel/NewMessageViewModel.swift
@@ -0,0 +1,38 @@
+//
+//  NewMessageViewModel.swift
+//  ChatAppSUI
+//
+//  Created by Emre İÇMEN on 18.12.2024.
+//
+
+import Foundation
+import SwiftUI
+import FirebaseFirestore
+
+
+class NewMessageViewModel: ObservableObject {
+    
+    @Published var users = [User]()
+    
+    init() {
+        fetchUsers()
+    }
+    
+    func fetchUsers() {
+        COLLECTION_USERS.getDocuments { snapshot, _ in
+            guard let documents = snapshot?.documents else { return }
+                
+            
+//            documents.forEach { document in
+//                guard let user = try? document.data(as: User.self) else {return}
+//                self.users.append(user)
+//            }
+            
+            //This is the shotrcut for appending users to users array
+            self.users = documents.compactMap({ try? $0.data(as: User.self) })
+            
+            print("DEBUG: Users: \(self.users)")
+
+        }
+    }
+}

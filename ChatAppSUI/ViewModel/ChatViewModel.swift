664f88911679fc9a68fcab130efcc4e0e1cf72ef
Emre İçmen <icmenemre@gmail.com>
2024-12-19 13:04:47 +0300

    Load messages in realtime and sort the messages

--- ChatAppSUI/ViewModel/ChatViewModel.swift
+++ ChatAppSUI/ViewModel/ChatViewModel.swift
@@ -24,42 +24,17 @@ class ChatViewModel: ObservableObject {
         guard let currentUid = AuthViewModel.shared.userSession?.uid else { return }
         guard let chatPartnerId = user.id else { return }
         
-        let query = COLLECTION_MESSAGES.document(currentUid).collection(chatPartnerId)
+        let query = COLLECTION_MESSAGES.document(currentUid).collection(chatPartnerId).order(by: "timestamp", descending: false)
         
-        query.getDocuments { snapshot, error in
-            if let error = error {
-                print("Error fetching documents: \(error)")
-                return
+        //For realtime chatting
+        query.addSnapshotListener { snapshot, _ in
+            guard let changes = snapshot?.documentChanges.filter({ $0.type == .added }) else { return }
+            
+            var messages = changes.compactMap { try? $0.document.data(as: Message.self) }
+            for (index, message) in messages.enumerated() where message.fromId != currentUid{
+                messages[index].user = self.user
             }
-
-            guard let documents = snapshot?.documents else {
-                print("No documents found.")
-                return
-            }
-
-            do {
-                self.messages = try documents.compactMap { document in
-                    try document.data(as: Message.self)
-                }
-                
-                // Kullanıcının ID'sine göre user ekleme
-                for (index, message) in self.messages.enumerated() where message.fromId != currentUid {
-                    self.messages[index].user = self.user
-                }
-                for message in self.messages {
-                    print("golem: \(message)")
-                }
-                
-                // Gönderim zamanına göre sıralama
-                self.messages.sort {
-                    let date1 = $0.timestamp.dateValue()
-                    let date2 = $1.timestamp.dateValue()
-                    return date1 < date2
-                }
-                
-            } catch {
-                print("Error decoding documents: \(error)")
-            }
+            self.messages.append(contentsOf: messages)
         }
     }
     
